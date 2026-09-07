f4b1b29a371852994a95567d98c838c7db1014ec
fsa00826 <fsa00826@gmail.com>
2026-09-07 12:31:15 +0200

    s

---
+++ Program.cs
@@ -0,0 +1,12 @@
+
+
+string Niel = "Niel Armstrong ";
+string Armstrong = "famously said when he landed on the moon: ";
+
+/*
+Console.BackgroundColor = ConsoleColor.pink;
+Console.ForegroundColor = ConsoleColor.black;
+*/
+Console.WriteLine(Niel + Armstrong);
+
+Console.WriteLine("That's one small step for man, another giant leap for mankind");
