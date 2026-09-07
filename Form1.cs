dbbef37edfc2500dc27bf33f1997ad180809efbb
fsa00826 <fsa00826@gmail.com>
2026-09-07 14:27:35 +0200

    Ja

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Data;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void one_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
