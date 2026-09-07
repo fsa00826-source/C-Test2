9f4bacd620ba13cab511d18b3f469360ea744427
fsa00826 <fsa00826@gmail.com>
2026-09-07 12:13:42 +0200

    Ja

---
+++ Main.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+if (Input.GetKeyDown(KeyCode.ESC))
+{
+    exit(program);
+}
+else
+{
+    return null;
+}
+
+public class Main
+{
+    public string MinEiendom => "VALUE";
+
+    public string MinEiendomA
+    {
+        get { return "VALUE"; }
+        set { null; }
+    }
+}
